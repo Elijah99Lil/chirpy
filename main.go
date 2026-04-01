6e22be1987d6c6ebb7cf5b090612a8a17ac5d8ba
Elijah99Lil <elijah99lil@gmail.com>
2026-04-01 01:36:37 -0600

    II: Middleware created and wired for Metrics and Reset.

--- main.go
+++ main.go
@@ -1,17 +1,59 @@
+//If you want to compile a binary and run the server:
+// go build -o out && ./out
+
 package main
 
 import (
 	"net/http"
 	"fmt"
+	"sync/atomic"
 )
 
+type apiConfig struct {
+	fileServerHits atomic.Int32
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileServerHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
+
+func handlerHealthz(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("OK"))
+}
+
+func (cfg *apiConfig) handlerMetrics(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	value := cfg.fileServerHits.Load()
+	text := fmt.Sprintf("Hits: %v", value)
+	w.Write([]byte(text))
+}
+
+func (cfg *apiConfig) handlerReset(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	cfg.fileServerHits.Store(0)
+	w.Write([]byte("Reset"))
+}
+
 func main() {
+	apiCfg := apiConfig{}
 	mux := http.NewServeMux()
+	mux.HandleFunc("GET /healthz", handlerHealthz)
+	mux.HandleFunc("GET /metrics", apiCfg.handlerMetrics)
+	mux.HandleFunc("POST /reset", apiCfg.handlerReset)
 
 	server := &http.Server{
 		Addr:		":8080",
 		Handler:	mux,
 	}
+	
+	mux.Handle("/app/", apiCfg.middlewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir(".")))))
 
 	err := server.ListenAndServe() 
 	if err != nil {
