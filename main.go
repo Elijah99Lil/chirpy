2e4578ba6cdf69a8ef57cdcf526fbf1d36f8f3c8
Elijah99Lil <elijah99lil@gmail.com>
2026-04-15 04:09:08 -0600

    IV: Verified Chirps to a set length, added a very strange word filter and set up a Chirpy SQL Database and wirings for Schemas and Queries.

--- main.go
+++ main.go
@@ -4,9 +4,13 @@
 package main
 
 import (
-	"net/http"
+	"encoding/json"
 	"fmt"
+	"log"
+	"net/http"
+	"strings"
 	"sync/atomic"
+	_ "github.com/lib/pq"
 )
 
 type apiConfig struct {
@@ -47,12 +51,72 @@ func (cfg *apiConfig) handlerReset(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Reset"))
 }
 
+func handlerValidateChirp(w http.ResponseWriter, r *http.Request) {
+	type requestBody struct {
+		Body	string	`json:"body"`
+	}
+	type responseBody struct {
+		CleanedBody	string	`json:"cleaned_body"`
+	}
+
+	decoder := json.NewDecoder(r.Body)
+	params := requestBody{}
+	err := decoder.Decode(&params)
+    if err != nil {
+		log.Printf("Error decoding parameters: %s", err)
+		w.WriteHeader(500)
+		return
+    }
+
+	if len(params.Body) > 140 {
+		respondWithError(w, 400, "Chirp is too long")
+		return
+	}
+
+	cleaned := stringCleaner(params.Body)
+	respondWithJSON(w, 200, responseBody{
+		CleanedBody: cleaned,
+	})
+}
+
+func stringCleaner(body string) string {
+	badWords := []string{"kerfuffle", "sharbert", "fornax"}
+	words := strings.Split(body, " ")
+	for i, word := range words {
+		lowered := strings.ToLower(word)
+		for _, badWord := range badWords {
+			if lowered == badWord {
+				words[i] = "****"
+			}
+		}
+	}
+	return strings.Join(words, " ")
+}
+	
+
+func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) error {
+	response, err := json.Marshal(payload)
+	if err != nil {
+		return err
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.WriteHeader(code)
+	w.Write(response)
+	return nil
+}
+
+func respondWithError(w http.ResponseWriter, code int, msg string) error {
+	return respondWithJSON(w, code, map[string]string{"error":msg})
+}
+
 func main() {
 	apiCfg := apiConfig{}
 	mux := http.NewServeMux()
 	mux.HandleFunc("GET /api/healthz", handlerHealthz)
 	mux.HandleFunc("GET /admin/metrics", apiCfg.handlerMetrics)
 	mux.HandleFunc("POST /admin/reset", apiCfg.handlerReset)
+	mux.HandleFunc("POST /api/validate_chirp", handlerValidateChirp)
 
 	server := &http.Server{
 		Addr:		":8080",
